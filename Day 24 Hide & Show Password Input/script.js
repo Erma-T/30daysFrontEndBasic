f5e6ec5baac6b140094ba4124f174be3d93036e7
erma-T <ermy143@gmail.com>
2024-06-24 23:12:33 +0300

    Script fix - eye icon change added

--- Day 24 Hide & Show Password Input/script.js
+++ Day 24 Hide & Show Password Input/script.js
@@ -2,5 +2,13 @@ const input = document.querySelector("input");
 const eye = document.querySelector("#eye-icon");
 
 eye.addEventListener("click", () => {
-  input.type === "password" ? (input.type = "text") : (input.type = "password");
+  input.type === "password" ? 
+    ((input.type = "text"),
+    eye.classList.remove("fa-eye"),
+    eye.classList.add("fa-eye-slash"))
+    
+    : ((input.type = "password"),
+    eye.classList.remove("fa-eye-slash"),
+    eye.classList.add("fa-eye"));
+  eye.class
 });
